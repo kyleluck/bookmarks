85f8d97520c1ca164bd54c5eb3da133fc12c7ee9
Kyle Luck <luck.kyle@gmail.com>
2016-07-07 09:49:01 -0400

    refactoring /save with promises

--- index.js
+++ index.js
@@ -24,34 +24,27 @@ app.post('/save', function(req, res) {
   var link = req.body.link;
 
   // see if bookmark exists by link
-  Bookmark.findOne(
-    { link: link },
-    function(err, bookmark) {
-      if (err) {
-        res.status(400).send({ "status": "fail", "message": "There was an error checking for existing bookmark: " + err.message });
-        return;
-      }
+  Bookmark.findOne({ link: link })
+    .then(function(bookmark) {
       if (bookmark) {
-        res.status(400).send({ "status": "fail", "message": "There is already a bookmark saved with this link" });
-        return;
-      } else {
-        // no duplicate found, save the new bookmark
-        var newbookmark = new Bookmark({
-          title: title,
-          link: link
-        });
-
-        // save the new bookmark to the database
-        newbookmark.save(function(err) {
-          if (err) {
-            res.status(400).send({ "status": "fail", "message": "There was an error saving the bookmark : " + err.message });
-            return;
-          }
-          res.status(200).send({ "status": "ok", "message": "Bookmark saved" });
-        });
+        //we've found a duplicate, throw error
+        throw new Error('Duplicate bookmark');
       }
-    }
-  );
+      // no duplicate found, save the new bookmark
+      var newbookmark = new Bookmark({
+        title: title,
+        link: link
+      });
+
+      // save the new bookmark to the database
+      return newbookmark.save();
+    })
+    .then(function() {
+      res.status(200).send({ "status": "ok", "message": "Bookmark saved" });
+    })
+    .catch(function(err) {
+      res.status(400).send({ "status": "fail", "message": "There was an error: " + err.message });
+    });
 });
 
 // view saved bookmarks
